bdc31a585661b45c50d6c09b9ed1a2e6ab05270f
SeleneGonzalo <selenegonzalo89@gmail.com>
2022-06-14 12:15:59 -0300

    conexion app a API para obtener categorias

--- src/layouts/CreateRecipe.js
+++ src/layouts/CreateRecipe.js
@@ -2,16 +2,52 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import TemplateRecipe from "../components/TemplateRecipe"
 import IngredientsData from "../data/IngredientsData" 
-import CategoriesData from "../data/CategoriesData"
+import {useState, useEffect } from 'react'
 
-function CreateRecipe(){
+export default function CreateRecipe(){
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        getCategories()
+    }, []);
+
+    function getCategories(){
+        setError(false);
+        fetch('https://api-recetaccs.herokuapp.com/categories', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        }).then(res => res.ok ? res.json() : null)
+        .then(
+            (data) => {
+                if(data !== null){
+                    setCategories(data)
+                    setIsLoaded(true)
+                }else{
+                    setError(true);
+                    setIsLoaded(true);
+                }
+            },
+        )
+    }
     return (
         <div className = "flex flex-col h-screen justify-between">
             <Header />
-            <TemplateRecipe ingredients={IngredientsData} categories={CategoriesData}/>
+            {isLoaded ? 
+                !error ?
+                    <TemplateRecipe ingredients={IngredientsData} categories={categories}/>
+                :
+                <h1> ERROR </h1>
+            :
+                <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-yellow-500 mx-auto" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            }
             <Footer/> 
         </div>
     )
 }
-
-export default CreateRecipe
--- src/layouts/Recipes.js
+++ src/layouts/Recipes.js
@@ -7,7 +7,7 @@ function Recipes (){
     return (
         <div className = "flex flex-col h-screen justify-between">
             <Header />
-            <div className="my-auto grid 2xl:grid-cols-5 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 mx-auto">
+            <div className="my-auto grid 2xl:grid-cols-5 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 mt-4">
                 {recipes.recipes.map(((recipe) => (
                     <RecipeCard recipe={recipe} key={recipe.id}/>
                 )))}
